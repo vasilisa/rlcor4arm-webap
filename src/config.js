6bbaa2ae4091b2e75cf7afb43a97b9f31b7f3ac6
vasilisa <vasilisaskv@gmail.com>
2021-10-08 22:44:53 +0100

    changing API url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const API_URL='https://rlcor4arm.osc-fr1.scalingo.io'
-export const API_URL='http://127.0.0.1:5000'
+export const API_URL='https://rlcor4arm-api.osc-fr1.scalingo.io'
+// export const API_URL='http://127.0.0.1:5000'
